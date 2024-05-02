c235238458bf6fc193a313ef0f54ced19eda8e16
Kama2111 <153067902+Kamran2111@users.noreply.github.com>
2024-05-02 21:06:13 +0400

    first commit

---
+++ js/hw-26.js
@@ -0,0 +1,19 @@
+// ЗАДАНИЕ: 1
+// const inputRange = document.querySelector(".input-range");
+// const inputRangeText = document.querySelector(".input-range__text");
+
+// inputRange.addEventListener("input", () => {
+//   inputRangeText.textContent = inputRange.value;
+// });
+// ЗАДАНИЕ: 2
+// let sliderScroll = document.querySelector(".slider");
+// let leftBtn = document.querySelector(".left");
+// let rightBtn = document.querySelector(".right");
+// rightBtn.addEventListener("click", () => {
+//   sliderScroll.style.scrollBehavior = "smooth";
+//   sliderScroll.scrollLeft += 900;
+// });
+// leftBtn.addEventListener("click", () => {
+//   sliderScroll.style.scrollBehavior = "smooth";
+//   sliderScroll.scrollLeft -= 900;
+// });
